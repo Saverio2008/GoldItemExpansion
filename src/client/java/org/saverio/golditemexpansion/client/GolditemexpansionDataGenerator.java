97f8ca39d597b61592d2a8c17658be7a504dc3ec
Saverio08 <situyuhui2008@gmail.com>
2025-07-04 18:05:40 +0200

    提交进度

--- src/client/java/org/saverio/golditemexpansion/client/GolditemexpansionDataGenerator.java
+++ src/client/java/org/saverio/golditemexpansion/client/GolditemexpansionDataGenerator.java
@@ -2,64 +2,11 @@ package org.saverio.golditemexpansion.client;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
-import net.fabricmc.fabric.api.datagen.v1.provider.FabricLanguageProvider;
-import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
-import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
-import net.minecraft.data.server.recipe.RecipeJsonProvider;
-import net.minecraft.item.Items;
-import org.saverio.golditemexpansion.item.ModItems;
-
-import java.util.function.Consumer;
 
 public class GolditemexpansionDataGenerator implements DataGeneratorEntrypoint {
 
     @Override
     public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
         FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
-        pack.addProvider(new LanguageProvider(pack));
-        pack.addProvider(new RecipeProvider(pack));
-        pack.addProvider(new ModelProvider(pack));
-    }
-
-    private static class LanguageProvider extends FabricLanguageProvider {
-        public LanguageProvider(FabricDataGenerator.Pack pack) {
-            super(pack);
-        }
-
-        @Override
-        public void generateTranslations(TranslationBuilder translationBuilder) {
-            translationBuilder.add(ModItems.COMPRESSED_GOLD_BLOCK, "压缩金块");
-        }
-    }
-
-    private static class RecipeProvider extends FabricRecipeProvider {
-        public RecipeProvider(FabricDataGenerator.Pack pack) {
-            super(pack);
-        }
-
-        @Override
-        public void generate(Consumer<RecipeJsonProvider> exporter) {
-            offerShapedRecipe(exporter, ModItems.COMPRESSED_GOLD_BLOCK,
-                    "GGG",
-                    "GGG",
-                    "GGG",
-                    'G', Items.GOLD_BLOCK);
-        }
-    }
-
-    private static class ModelProvider extends FabricModelProvider {
-        public ModelProvider(FabricDataGenerator.Pack pack) {
-            super(pack);
-        }
-
-        @Override
-        public void generateBlockStateModels() {
-            // 此处不需要方块模型（非方块物品）
-        }
-
-        @Override
-        public void generateItemModels() {
-            itemModelGenerated(ModItems.COMPRESSED_GOLD_BLOCK);
-        }
     }
 }
--- src/main/java/org/saverio/golditemexpansion/item/ModItems.java
+++ src/main/java/org/saverio/golditemexpansion/item/ModItems.java
@@ -1,17 +1,18 @@
 package org.saverio.golditemexpansion.item;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 import org.saverio.golditemexpansion.Golditemexpansion;
+import org.saverio.golditemexpansion.block.ModBlocks;
 
 public class ModItems {
-    public static final Item COMPRESSED_GOLD_BLOCK = new Item(new FabricItemSettings());
     public static void registerItems() {
+        // 注册能放置方块的物品（BlockItem）
         Registry.register(Registries.ITEM,
                 new Identifier(Golditemexpansion.MOD_ID, "compressed_gold_block"),
-                COMPRESSED_GOLD_BLOCK);
+                new BlockItem(ModBlocks.COMPRESSED_GOLD_BLOCK, new FabricItemSettings()));
     }
 }
