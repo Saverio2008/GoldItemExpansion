5a878243bc35879d366aed6e72fdba712b3b4cc3
Saverio08 <situyuhui2008@gmail.com>
2025-07-04 21:39:25 +0200

    提交进度

--- src/main/java/org/saverio/golditemexpansion/block/GoldenHeadBlock.java
+++ src/main/java/org/saverio/golditemexpansion/block/GoldenHeadBlock.java
@@ -16,36 +16,39 @@ import java.util.List;
 
 public class GoldenHeadBlock extends Block {
 
+    private static final int TICKS_BEFORE_REMOVE = 40; // 2秒 (20 ticks = 1秒)
+
     public GoldenHeadBlock(Settings settings) {
         super(settings);
     }
 
-    // 放置时调用，或者用 scheduled tick 定时给玩家加效果
     @Override
     public void onPlaced(World world, BlockPos pos, BlockState state, LivingEntity placer, ItemStack itemStack) {
         super.onPlaced(world, pos, state, placer, itemStack);
+
         if (!world.isClient) {
-            applyEffectsToNearbyPlayers((ServerWorld) world, pos);
+            ServerWorld serverWorld = (ServerWorld) world;
+
+            applyEffectsToNearbyPlayers(serverWorld, pos);
+
+            serverWorld.scheduleBlockTick(pos, this, TICKS_BEFORE_REMOVE);
         }
     }
 
     private void applyEffectsToNearbyPlayers(ServerWorld world, BlockPos pos) {
-        // 范围半径，比如 10 格方块范围
-        int radius = 10;
-        Box area = new Box(pos).expand(radius);
+        Box area = new Box(
+                pos.getX() - 10, pos.getY() - 1, pos.getZ() - 10,
+                pos.getX() + 10, pos.getY() + 1, pos.getZ() + 10
+        );
 
         List<PlayerEntity> players = world.getEntitiesByClass(PlayerEntity.class, area, player -> true);
 
-        int durationTicks = 6000;
+        int durationTicks = 6000; // 5分钟
 
         for (PlayerEntity player : players) {
-            // 抗性VII (level 6，因为level从0开始计)
             player.addStatusEffect(new StatusEffectInstance(StatusEffects.RESISTANCE, durationTicks, 6));
-            // 抗火 (level 0)
             player.addStatusEffect(new StatusEffectInstance(StatusEffects.FIRE_RESISTANCE, durationTicks, 0));
-            // 伤害吸收X (level 9)
             player.addStatusEffect(new StatusEffectInstance(StatusEffects.ABSORPTION, durationTicks, 9));
-            // 生命提升X (level 9)
             player.addStatusEffect(new StatusEffectInstance(StatusEffects.HEALTH_BOOST, durationTicks, 9));
         }
     }
