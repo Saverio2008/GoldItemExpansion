4287ab9c841f90e60feb27e4cd1009e4a61c17e3
Saverio08 <situyuhui2008@gmail.com>
2025-07-04 19:09:31 +0200

    提交进度

--- src/main/java/org/saverio/golditemexpansion/item/ModItems.java
+++ src/main/java/org/saverio/golditemexpansion/item/ModItems.java
@@ -1,7 +1,8 @@
 package org.saverio.golditemexpansion.item;
 
-import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.BlockItem;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -10,9 +11,9 @@ import org.saverio.golditemexpansion.block.ModBlocks;
 
 public class ModItems {
     public static void registerItems() {
-        // 注册能放置方块的物品（BlockItem）
         Registry.register(Registries.ITEM,
                 new Identifier(Golditemexpansion.MOD_ID, "compressed_gold_block"),
-                new BlockItem(ModBlocks.COMPRESSED_GOLD_BLOCK, new FabricItemSettings()));
+                new BlockItem(ModBlocks.COMPRESSED_GOLD_BLOCK, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS))
+        );
     }
 }
