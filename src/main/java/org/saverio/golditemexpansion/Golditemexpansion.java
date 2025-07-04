29cc1c9c5df38a63f806b53a1dd30e7ad3a57de0
Saverio08 <situyuhui2008@gmail.com>
2025-07-04 21:58:50 +0200

    提交进度

--- src/main/java/org/saverio/golditemexpansion/Golditemexpansion.java
+++ src/main/java/org/saverio/golditemexpansion/Golditemexpansion.java
@@ -1,7 +1,5 @@
 package org.saverio.golditemexpansion;
 
-import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
-import net.minecraft.item.ItemGroups;
 import org.saverio.golditemexpansion.item.ModItems;
 import org.saverio.golditemexpansion.block.ModBlocks;
 import net.fabricmc.api.ModInitializer;
@@ -12,7 +10,5 @@ public class Golditemexpansion implements ModInitializer {
     public void onInitialize() {
         ModItems.registerItems();
         ModBlocks.registerBlocks();
-        ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(content ->
-                content.addAfter(net.minecraft.block.Blocks.GOLD_BLOCK.asItem(), ModItems.COMPRESSED_GOLD_BLOCK_ITEM));
     }
 }
--- src/main/java/org/saverio/golditemexpansion/item/ModItems.java
+++ src/main/java/org/saverio/golditemexpansion/item/ModItems.java
@@ -1,8 +1,10 @@
 package org.saverio.golditemexpansion.item;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -22,5 +24,7 @@ public class ModItems {
         Registry.register(Registries.ITEM,
                 new Identifier(Golditemexpansion.MOD_ID, "golden_head"),
                 GOLDEN_HEAD_ITEM);
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(content ->
+                content.addAfter(net.minecraft.block.Blocks.GOLD_BLOCK.asItem(), ModItems.COMPRESSED_GOLD_BLOCK_ITEM));
     }
 }
