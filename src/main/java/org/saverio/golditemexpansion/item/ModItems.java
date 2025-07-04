0d8a52e5220d4cd35eea06b6e6decc20897e93b7
Saverio08 <situyuhui2008@gmail.com>
2025-07-04 19:13:19 +0200

    提交进度

--- src/main/java/org/saverio/golditemexpansion/item/ModItems.java
+++ src/main/java/org/saverio/golditemexpansion/item/ModItems.java
@@ -1,7 +1,7 @@
 package org.saverio.golditemexpansion.item;
 
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.BlockItem;
-import net.minecraft.item.ItemGroup;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
@@ -10,10 +10,12 @@ import org.saverio.golditemexpansion.Golditemexpansion;
 import org.saverio.golditemexpansion.block.ModBlocks;
 
 public class ModItems {
+    public static final Item COMPRESSED_GOLD_BLOCK_ITEM = new BlockItem(ModBlocks.COMPRESSED_GOLD_BLOCK, new FabricItemSettings());
+
     public static void registerItems() {
         Registry.register(Registries.ITEM,
                 new Identifier(Golditemexpansion.MOD_ID, "compressed_gold_block"),
-                new BlockItem(ModBlocks.COMPRESSED_GOLD_BLOCK, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS))
+                COMPRESSED_GOLD_BLOCK_ITEM
         );
     }
 }
