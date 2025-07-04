cda7d7fd9e021113a975dee6b9f95932a5e92cb7
Saverio08 <situyuhui2008@gmail.com>
2025-07-04 10:58:36 +0200

    Initial commit

---
+++ src/client/java/org/saverio/golditemexpansion/client/GolditemexpansionClient.java
@@ -0,0 +1,10 @@
+package org.saverio.golditemexpansion.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GolditemexpansionClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
