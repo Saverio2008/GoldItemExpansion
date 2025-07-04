467ecd22466356e4839b07a7de3855af095a0566
Saverio08 <situyuhui2008@gmail.com>
2025-07-04 18:37:23 +0200

    提交进度

--- src/main/java/org/saverio/golditemexpansion/block/ModBlocks.java
+++ src/main/java/org/saverio/golditemexpansion/block/ModBlocks.java
@@ -2,6 +2,7 @@ package org.saverio.golditemexpansion.block;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Blocks;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -9,8 +10,7 @@ import org.saverio.golditemexpansion.Golditemexpansion;
 
 public class ModBlocks {
     public static final Block COMPRESSED_GOLD_BLOCK = new Block(
-            AbstractBlock.Settings
-                    .create()
+            AbstractBlock.Settings.copy(Blocks.GOLD_BLOCK)
                     .strength(8.0f, 1200.0f)
     );
     public static void registerBlocks() {
