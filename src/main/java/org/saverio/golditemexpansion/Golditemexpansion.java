6c921ccd7831616cd65b441a995a8a79ce98be17
Saverio08 <situyuhui2008@gmail.com>
2025-07-04 10:58:36 +0200

    提交进度

---
+++ src/main/java/org/saverio/golditemexpansion/Golditemexpansion.java
@@ -0,0 +1,10 @@
+package org.saverio.golditemexpansion;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Golditemexpansion implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
