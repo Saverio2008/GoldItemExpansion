e0e45780cac816fccc250cec9e066f90c29c6bdd
Saverio08 <situyuhui2008@gmail.com>
2025-07-05 08:43:00 +0200

    提交进度

--- src/main/java/org/saverio/golditemexpansion/item/ModItems.java
+++ src/main/java/org/saverio/golditemexpansion/item/ModItems.java
@@ -2,6 +2,7 @@ package org.saverio.golditemexpansion.item;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.block.Blocks;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
@@ -25,7 +26,10 @@ public class ModItems {
                 new Identifier(Golditemexpansion.MOD_ID, "golden_head"),
                 GOLDEN_HEAD_ITEM);
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(content ->
-                content.addAfter(net.minecraft.block.Blocks.GOLD_BLOCK.asItem(),
+                content.addAfter(Blocks.GOLD_BLOCK.asItem(),
                         ModItems.COMPRESSED_GOLD_BLOCK_ITEM));
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FUNCTIONAL).register(content ->
+                content.addAfter(Blocks.PIGLIN_HEAD.asItem(), ModItems.GOLDEN_HEAD_ITEM)
+        );
     }
 }
