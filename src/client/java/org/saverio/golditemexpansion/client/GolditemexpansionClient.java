9f9f63ccd8eacdaaebc45db412d4616ae910c514
Saverio08 <situyuhui2008@gmail.com>
2025-07-05 09:13:58 +0200

    提交进度

--- src/client/java/org/saverio/golditemexpansion/client/GolditemexpansionClient.java
+++ src/client/java/org/saverio/golditemexpansion/client/GolditemexpansionClient.java
@@ -1,20 +1,9 @@
 package org.saverio.golditemexpansion.client;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry;
-import net.minecraft.client.color.block.BlockColorProvider;
-import net.minecraft.client.color.item.ItemColorProvider;
-import org.saverio.golditemexpansion.block.ModBlocks;
-import org.saverio.golditemexpansion.item.ModItems;
 
 public class GolditemexpansionClient implements ClientModInitializer {
-    public static final int GOLD_TINT = 0xFFFFD700;
     @Override
     public void onInitializeClient() {
-        BlockColorProvider goldenHeadBlockColorProvider =
-                (state, world, pos, tintIndex) -> GOLD_TINT;
-        ItemColorProvider goldenHeadItemColorProvider = (stack, tintIndex) -> GOLD_TINT;
-        ColorProviderRegistry.BLOCK.register(goldenHeadBlockColorProvider, ModBlocks.GOLDEN_HEAD_BLOCK);
-        ColorProviderRegistry.ITEM.register(goldenHeadItemColorProvider, ModItems.GOLDEN_HEAD_ITEM);
     }
 }
