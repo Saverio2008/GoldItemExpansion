c866f0abc535e2f33f6cff38a46849d80405cf99
Saverio08 <situyuhui2008@gmail.com>
2025-07-05 17:56:03 +0200

    提交进度

--- src/main/java/org/saverio/golditemexpansion/block/GoldenHeadBlock.java
+++ src/main/java/org/saverio/golditemexpansion/block/GoldenHeadBlock.java
@@ -3,28 +3,44 @@ package org.saverio.golditemexpansion.block;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.HorizontalFacingBlock;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.item.ItemStack;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.DirectionProperty;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Box;
+import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.world.World;
 
 import java.util.List;
 
 public class GoldenHeadBlock extends Block {
+    public static final DirectionProperty FACING = HorizontalFacingBlock.FACING;
     private static final int TICKS_BEFORE_REMOVE = 40;
+
     public GoldenHeadBlock(Settings settings) {
         super(settings);
+        setDefaultState(this.getStateManager().getDefaultState().with(FACING, Direction.NORTH));
+    }
+    @Override
+    public BlockState getPlacementState(ItemPlacementContext ctx) {
+        return this.getDefaultState().with(FACING, ctx.getHorizontalPlayerFacing().getOpposite());
+    }
+    // 注册 FACING 属性
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
     }
     @Override
     public void onPlaced(World world, BlockPos pos, BlockState state, LivingEntity placer, ItemStack itemStack) {
         super.onPlaced(world, pos, state, placer, itemStack);
-
         if (!world.isClient) {
             ServerWorld serverWorld = (ServerWorld) world;
             applyEffectsToNearbyPlayers(serverWorld, pos);
