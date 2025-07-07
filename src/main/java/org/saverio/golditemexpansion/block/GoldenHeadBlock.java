7056cd6ba3cde881d9287f2ae51ed420c08f8f8d
Saverio08 <situyuhui2008@gmail.com>
2025-07-07 09:00:22 +0200

    提交进度

--- src/main/java/org/saverio/golditemexpansion/block/GoldenHeadBlock.java
+++ src/main/java/org/saverio/golditemexpansion/block/GoldenHeadBlock.java
@@ -1,6 +1,9 @@
 package org.saverio.golditemexpansion.block;
 
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.SkullBlock;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
@@ -18,13 +21,12 @@ public class GoldenHeadBlock extends SkullBlock {
     private static final int EFFECT_RADIUS = 10;
 
     public GoldenHeadBlock(Settings settings) {
-        // 用一个自定义的 SkullType，贴图绑定时会用它的注册名
-        super(SkullBlock.Type.SKELETON, settings);
+        super(SkullBlock.Type.SKELETON, settings); // 保持动态渲染
     }
 
     @Override
-    public void onPlaced(World world, BlockPos pos, net.minecraft.block.BlockState state, net.minecraft.entity.LivingEntity placer, ItemStack itemStack) {
-        super.onPlaced(world, pos, state, placer, itemStack);
+    public void onPlaced(World world, BlockPos pos, BlockState state, LivingEntity placer, ItemStack stack) {
+        super.onPlaced(world, pos, state, placer, stack);
         if (!world.isClient) {
             ServerWorld serverWorld = (ServerWorld) world;
             applyEffectsToNearbyPlayers(serverWorld, pos);
@@ -34,8 +36,8 @@ public class GoldenHeadBlock extends SkullBlock {
 
     @SuppressWarnings("deprecation")
     @Override
-    public void scheduledTick(net.minecraft.block.BlockState state, ServerWorld world, BlockPos pos, Random random) {
-        world.setBlockState(pos, net.minecraft.block.Blocks.AIR.getDefaultState());
+    public void scheduledTick(BlockState state, ServerWorld world, BlockPos pos, Random random) {
+        world.setBlockState(pos, Blocks.AIR.getDefaultState());
     }
 
     private void applyEffectsToNearbyPlayers(ServerWorld world, BlockPos pos) {
@@ -44,7 +46,7 @@ public class GoldenHeadBlock extends SkullBlock {
                 pos.getX() + EFFECT_RADIUS, pos.getY() + EFFECT_RADIUS, pos.getZ() + EFFECT_RADIUS
         );
 
-        List<PlayerEntity> players = world.getEntitiesByClass(PlayerEntity.class, area, player -> true);
+        List<PlayerEntity> players = world.getEntitiesByClass(PlayerEntity.class, area, p -> true);
         int durationTicks = 6000;
 
         for (PlayerEntity player : players) {
@@ -58,16 +60,9 @@ public class GoldenHeadBlock extends SkullBlock {
     private void applyOrExtendEffect(PlayerEntity player, StatusEffectInstance newEffect) {
         StatusEffectInstance current = player.getStatusEffect(newEffect.getEffectType());
         if (current != null) {
-            int extendedDuration = current.getDuration() + newEffect.getDuration();
-            int amplifier = Math.max(current.getAmplifier(), newEffect.getAmplifier());
-            player.addStatusEffect(new StatusEffectInstance(
-                    newEffect.getEffectType(),
-                    extendedDuration,
-                    amplifier,
-                    newEffect.isAmbient(),
-                    newEffect.shouldShowParticles(),
-                    newEffect.shouldShowIcon()
-            ));
+            int extended = current.getDuration() + newEffect.getDuration();
+            int amp = Math.max(current.getAmplifier(), newEffect.getAmplifier());
+            player.addStatusEffect(new StatusEffectInstance(newEffect.getEffectType(), extended, amp));
         } else {
             player.addStatusEffect(newEffect);
         }
