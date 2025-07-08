dea6ef0fc3d2d67f4dcc9bdbc73449e8c2b23aad
Saverio08 <situyuhui2008@gmail.com>
2025-07-08 15:50:43 +0200

    提交进度

--- src/client/java/org/saverio/golditemexpansion/client/GolditemexpansionClient.java
+++ src/client/java/org/saverio/golditemexpansion/client/GolditemexpansionClient.java
@@ -1,18 +1,9 @@
 package org.saverio.golditemexpansion.client;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.minecraft.block.Block;
-import net.minecraft.block.entity.BlockEntityType;
-import org.saverio.golditemexpansion.client.mixin.BlockEntityTypeAccessor;
-import org.saverio.golditemexpansion.block.ModBlocks;
-
-import java.util.Set;
 
 public class GolditemexpansionClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        BlockEntityTypeAccessor skullAccessor = (BlockEntityTypeAccessor) BlockEntityType.SKULL;
-        Set<Block> set = skullAccessor.getBlocks();
-        set.add(ModBlocks.GOLDEN_HEAD_BLOCK);
     }
 }
