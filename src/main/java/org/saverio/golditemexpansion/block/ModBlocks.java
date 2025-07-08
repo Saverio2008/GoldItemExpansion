765f7aacb3ee4b7f4e35cf9a0e152f3aefb65104
Saverio08 <situyuhui2008@gmail.com>
2025-07-08 15:15:42 +0200

    提交进度

--- src/main/java/org/saverio/golditemexpansion/block/ModBlocks.java
+++ src/main/java/org/saverio/golditemexpansion/block/ModBlocks.java
@@ -3,19 +3,19 @@ package org.saverio.golditemexpansion.block;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.Registries;
 import net.minecraft.util.Identifier;
 import org.saverio.golditemexpansion.Golditemexpansion;
+import org.saverio.golditemexpansion.client.mixin.BlockEntityTypeAccessor;
 
 public class ModBlocks {
-    // 压缩金块方块定义，继承金块属性
     public static final Block COMPRESSED_GOLD_BLOCK = new Block(
             AbstractBlock.Settings.copy(Blocks.GOLD_BLOCK)
                     .strength(8.0f, 1200.0f)
     );
 
-    // 自定义金头方块，复制骷髅头属性
     public static final Block GOLDEN_HEAD_BLOCK = new GoldenHeadBlock(
             AbstractBlock.Settings.copy(Blocks.SKELETON_SKULL)
                     .strength(1.0f)
@@ -31,5 +31,9 @@ public class ModBlocks {
         Registry.register(Registries.BLOCK,
                 new Identifier(Golditemexpansion.MOD_ID, "golden_head"),
                 GOLDEN_HEAD_BLOCK);
+
+        // 通过 Mixin 访问器获得 SKULL 类型的 blocks 集合并添加自定义方块
+        BlockEntityTypeAccessor skullAccessor = (BlockEntityTypeAccessor) BlockEntityType.SKULL;
+        skullAccessor.getBlocks().add(GOLDEN_HEAD_BLOCK);
     }
 }
