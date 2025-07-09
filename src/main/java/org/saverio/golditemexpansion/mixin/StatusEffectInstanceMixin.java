659690eb8c53e5d45ba2fb0907127738a2250af4
Saverio08 <situyuhui2008@gmail.com>
2025-07-09 22:27:47 +0200

    提交进度

--- src/main/java/org/saverio/golditemexpansion/mixin/StatusEffectInstanceMixin.java
+++ src/main/java/org/saverio/golditemexpansion/mixin/StatusEffectInstanceMixin.java
@@ -4,10 +4,12 @@ import net.minecraft.entity.effect.StatusEffectInstance;
 import org.saverio.golditemexpansion.util.HiddenEffectInstance;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(StatusEffectInstance.class)
 public class StatusEffectInstanceMixin implements HiddenEffectInstance {
-
     @Unique
     private boolean goldItemExpansion$hideIcon = false;
 
@@ -19,5 +21,12 @@ public class StatusEffectInstanceMixin implements HiddenEffectInstance {
     @Override
     public void goldItemExpansion$setHideIcon(boolean hide) {
         this.goldItemExpansion$hideIcon = hide;
+    }
+
+    @Inject(method = "shouldShowIcon()Z", at = @At("HEAD"), cancellable = true)
+    private void onShouldShowIcon(CallbackInfoReturnable<Boolean> cir) {
+        if (goldItemExpansion$hideIcon) {
+            cir.setReturnValue(false);
+        }
     }
 }
