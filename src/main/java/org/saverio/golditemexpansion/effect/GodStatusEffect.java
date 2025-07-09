1fad1884fa7060394fa39c0777ff8d7769977502
Saverio08 <situyuhui2008@gmail.com>
2025-07-09 17:28:37 +0200

    提交进度

---
+++ src/main/java/org/saverio/golditemexpansion/effect/GodStatusEffect.java
@@ -0,0 +1,58 @@
+package org.saverio.golditemexpansion.effect;
+
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectCategory;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.player.PlayerEntity;
+
+import java.util.Objects;
+
+public class GodStatusEffect extends StatusEffect {
+    public GodStatusEffect() {
+        super(StatusEffectCategory.BENEFICIAL, 0xFF77FF);
+    }
+
+    @Override
+    public void applyUpdateEffect(LivingEntity entity, int amplifier) {
+        if (!entity.getWorld().isClient && entity.age % 20 == 0) {
+            int duration = Objects.requireNonNull(entity.getStatusEffect(this)).getDuration();
+
+            if (entity instanceof PlayerEntity) {
+                // 玩家：覆盖所有正面效果，不显示粒子和图标
+                replaceEffect(entity, StatusEffects.STRENGTH, duration, 4);
+                replaceEffect(entity, StatusEffects.RESISTANCE, duration, 4);
+                replaceEffect(entity, StatusEffects.REGENERATION, duration, 4);
+                replaceEffect(entity, StatusEffects.SPEED, duration, 4);
+                replaceEffect(entity, StatusEffects.HASTE, duration, 4);
+                replaceEffect(entity, StatusEffects.JUMP_BOOST, duration, 4);
+                replaceEffect(entity, StatusEffects.HEALTH_BOOST, duration, 4);
+                replaceEffect(entity, StatusEffects.FIRE_RESISTANCE, duration, 0);
+                replaceEffect(entity, StatusEffects.WATER_BREATHING, duration, 0);
+            } else {
+                // 非玩家：覆盖所有原版负面效果，不显示粒子和图标
+                replaceEffect(entity, StatusEffects.WEAKNESS, duration, 1);
+                replaceEffect(entity, StatusEffects.SLOWNESS, duration, 1);
+                replaceEffect(entity, StatusEffects.MINING_FATIGUE, duration, 1);
+                replaceEffect(entity, StatusEffects.UNLUCK, duration, 0);
+                replaceEffect(entity, StatusEffects.NAUSEA, duration, 0);
+                replaceEffect(entity, StatusEffects.BLINDNESS, duration, 0);
+                replaceEffect(entity, StatusEffects.HUNGER, duration, 1);
+                replaceEffect(entity, StatusEffects.DARKNESS, duration, 0);
+            }
+        }
+    }
+
+    private void replaceEffect(LivingEntity entity, StatusEffect effect, int duration, int amplifier) {
+        if (entity.hasStatusEffect(effect)) {
+            entity.removeStatusEffect(effect);
+        }
+        entity.addStatusEffect(new StatusEffectInstance(effect, duration, amplifier, false, false));
+    }
+
+    @Override
+    public boolean canApplyUpdateEffect(int duration, int amplifier) {
+        return true;
+    }
+}
