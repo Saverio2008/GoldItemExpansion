0da9c403ddf952a14d0e9940e16921799cd011d0
Saverio08 <situyuhui2008@gmail.com>
2025-07-09 16:35:14 +0200

    提交进度

---
+++ src/main/java/org/saverio/golditemexpansion/item/GodPotionItem.java
@@ -0,0 +1,11 @@
+package org.saverio.golditemexpansion.item;
+
+import net.minecraft.item.PotionItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+
+public class GodPotionItem extends PotionItem {
+    public GodPotionItem(Settings settings) {
+        super(settings);
+    }
+}
