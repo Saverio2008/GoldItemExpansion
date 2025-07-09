e2a303ab2eb514e7faa80b490c0f41782eacf3be
Saverio08 <situyuhui2008@gmail.com>
2025-07-09 22:47:41 +0200

    提交进度

--- src/main/java/org/saverio/golditemexpansion/util/EffectUtils.java
+++ src/main/java/org/saverio/golditemexpansion/util/EffectUtils.java
@@ -4,24 +4,25 @@ import net.minecraft.entity.effect.StatusEffectInstance;
 
 public class EffectUtils {
 
+    /**
+     * 设置一个状态效果图标是否应该在 GUI 中隐藏。
+     *
+     * @param instance 要修改的状态效果
+     * @param hide     是否隐藏图标
+     */
     public static void setHideIcon(StatusEffectInstance instance, boolean hide) {
         if (instance instanceof HiddenEffectInstance hidden) {
             hidden.goldItemExpansion$setHideIcon(hide);
         }
     }
 
-    private static boolean printedStack = false;
-
+    /**
+     * 检查状态效果是否被标记为隐藏。
+     *
+     * @param instance 状态效果实例
+     * @return 是否隐藏
+     */
     public static boolean shouldHideIcon(StatusEffectInstance instance) {
-        boolean hidden = instance instanceof HiddenEffectInstance h && h.goldItemExpansion$shouldHideIcon();
-        if (hidden) {
-            System.out.println("[GoldItemExpansion] Hiding icon for: " + instance.getEffectType().getTranslationKey());
-            if (!printedStack) {
-                printedStack = true;
-                System.out.println("[GoldItemExpansion] First stack trace for hidden icon:");
-                Thread.dumpStack();
-            }
-        }
-        return hidden;
+        return instance instanceof HiddenEffectInstance hidden && hidden.goldItemExpansion$shouldHideIcon();
     }
 }
