ed6bd7eeb29c7f8501b067808ed9acdb039e47ff
Saverio08 <situyuhui2008@gmail.com>
2025-07-10 21:50:35 +0200

    提交进度

--- src/client/java/org/saverio/golditemexpansion/client/mixin/StatusEffectSpriteManagerMixin.java
+++ src/client/java/org/saverio/golditemexpansion/client/mixin/StatusEffectSpriteManagerMixin.java
@@ -1,7 +1,6 @@
 package org.saverio.golditemexpansion.client.mixin;
 
 import net.minecraft.client.texture.Sprite;
-import net.minecraft.client.texture.SpriteAtlasTexture;
 import net.minecraft.client.texture.StatusEffectSpriteManager;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.registry.Registries;
@@ -14,20 +13,18 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(StatusEffectSpriteManager.class)
 public abstract class StatusEffectSpriteManagerMixin {
-
-    @SuppressWarnings("resource")
     @Inject(method = "getSprite", at = @At("HEAD"), cancellable = true)
     private void injectCustomSprite(StatusEffect effect, CallbackInfoReturnable<Sprite> cir) {
         Identifier id = Registries.STATUS_EFFECT.getId(effect);
         if (id != null && id.getNamespace().equals("golditemexpansion")) {
-            String path = id.getPath();
-            if (path.equals("god_positive_status_effect") || path.equals("god_negative_status_effect")) {
-                SpriteAtlasTexture atlas = ((SpriteAtlasHolderAccessor) this).golditemexpansion$getAtlas();
-                Identifier spriteId = new Identifier("golditemexpansion", "gui/status_effect/" + path);
-                Sprite sprite = atlas.getSprite(spriteId);
-                if (sprite != null) {
-                    cir.setReturnValue(sprite);
-                }
+            Identifier spriteId = switch (id.getPath()) {
+                case "god_positive_status_effect" -> new Identifier("golditemexpansion", "gui/status_effect/god_positive_status_effect");
+                case "god_negative_status_effect" -> new Identifier("golditemexpansion", "gui/status_effect/god_negative_status_effect");
+                default -> new Identifier("golditemexpansion", "gui/status_effect/god_default");
+            };
+            Sprite sprite = ((SpriteAtlasHolderAccessor) this).golditemexpansion$invokeGetSprite(spriteId);
+            if (sprite != null && !sprite.getContents().getId().getPath().contains("missing")) {
+                cir.setReturnValue(sprite);
             }
         }
     }
--- src/client/java/org/saverio/golditemexpansion/client/mixin/accessor/SpriteAtlasHolderAccessor.java
+++ src/client/java/org/saverio/golditemexpansion/client/mixin/accessor/SpriteAtlasHolderAccessor.java
@@ -1,12 +1,13 @@
 package org.saverio.golditemexpansion.client.mixin.accessor;
 
-import net.minecraft.client.texture.SpriteAtlasHolder;
-import net.minecraft.client.texture.SpriteAtlasTexture;
+import net.minecraft.client.texture.Sprite;
+import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.gen.Accessor;
+import org.spongepowered.asm.mixin.gen.Invoker;
+import net.minecraft.client.texture.SpriteAtlasHolder;
 
 @Mixin(SpriteAtlasHolder.class)
 public interface SpriteAtlasHolderAccessor {
-    @Accessor("atlas")
-    SpriteAtlasTexture golditemexpansion$getAtlas();
+    @Invoker("getSprite")
+    Sprite golditemexpansion$invokeGetSprite(Identifier id);
 }
