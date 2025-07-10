607df61ae6121746b4c66f931bc334c03a11c95d
Saverio08 <situyuhui2008@gmail.com>
2025-07-10 11:42:16 +0200

    提交进度

--- src/main/java/org/saverio/golditemexpansion/effect/ModEffects.java
+++ src/main/java/org/saverio/golditemexpansion/effect/ModEffects.java
@@ -1,5 +1,6 @@
 package org.saverio.golditemexpansion.effect;
 
+import net.minecraft.entity.effect.StatusEffectCategory;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.Registries;
 import net.minecraft.util.Identifier;
@@ -8,6 +9,7 @@ import org.saverio.golditemexpansion.Golditemexpansion;
 public class ModEffects {
     public static final GodPositiveStatusEffect GOD_POSITIVE_EFFECT = new GodPositiveStatusEffect();
     public static final GodNegativeStatusEffect GOD_NEGATIVE_EFFECT = new GodNegativeStatusEffect();
+    public static final GodStatusEffect GOD_STATUS_EFFECT = new GodStatusEffect(StatusEffectCategory.NEUTRAL, 0x000000);
 
     public static void registerEffects() {
         Registry.register(Registries.STATUS_EFFECT, new Identifier(Golditemexpansion.MOD_ID, "god_positive_status_effect"), GOD_POSITIVE_EFFECT);
