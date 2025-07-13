b346f4b1e65545f5bc6084807aa897503cb4031d
Saverio08 <situyuhui2008@gmail.com>
2025-07-13 19:26:59 +0200

    提交进度

--- src/main/java/org/saverio/golditemexpansion/potion/ModPotions.java
+++ src/main/java/org/saverio/golditemexpansion/potion/ModPotions.java
@@ -1,6 +1,7 @@
 package org.saverio.golditemexpansion.potion;
 
 import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.Item;
 import net.minecraft.item.PotionItem;
 import net.minecraft.potion.Potion;
@@ -18,6 +19,10 @@ public class ModPotions {
     public static Item UNDEAD_GOD_POTION_ITEM;
     public static Potion ARTHROPOD_GOD_POTION;
     public static Item ARTHROPOD_GOD_POTION_ITEM;
+    public static Potion HEALING_V;
+    public static Item HEALING_V_ITEM;
+    public static Potion GODLY_HEALING;
+    public static Item GODLY_HEALING_ITEM;
 
     public static void registerPotions() {
         // 注册药水
@@ -37,6 +42,16 @@ public class ModPotions {
                 new Potion(new StatusEffectInstance(ModEffects.GOD_STATUS_EFFECT,
                         3600, 2,false,true,false))
         );
+        HEALING_V = Registry.register(
+                Registries.POTION,
+                new Identifier(Golditemexpansion.MOD_ID, "strong_healing_v"),
+                new Potion(new StatusEffectInstance(StatusEffects.INSTANT_HEALTH, 1, 4))
+        );
+        GODLY_HEALING = Registry.register(
+                Registries.POTION,
+                new Identifier(Golditemexpansion.MOD_ID, "godly_healing"),
+                new Potion(new StatusEffectInstance(StatusEffects.INSTANT_HEALTH, 1, 14))
+        );
         // 注册药水物品
         GOD_POTION_ITEM = Registry.register(
                 Registries.ITEM,
@@ -51,6 +66,16 @@ public class ModPotions {
         ARTHROPOD_GOD_POTION_ITEM = Registry.register(
                 Registries.ITEM,
                 new Identifier(Golditemexpansion.MOD_ID, "arthropod_god_potion"),
+                new PotionItem(new Item.Settings().rarity(Rarity.RARE))
+        );
+        HEALING_V_ITEM = Registry.register(
+                Registries.ITEM,
+                new Identifier(Golditemexpansion.MOD_ID, "strong_healing_v"),
+                new PotionItem(new Item.Settings().rarity(Rarity.RARE))
+        );
+        GODLY_HEALING_ITEM = Registry.register(
+                Registries.ITEM,
+                new Identifier(Golditemexpansion.MOD_ID, "godly_healing"),
                 new PotionItem(new Item.Settings().rarity(Rarity.RARE))
         );
         ModBrewingRecipes.registerBrewingRecipes();
