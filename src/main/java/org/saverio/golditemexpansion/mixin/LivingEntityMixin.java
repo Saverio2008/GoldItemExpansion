913b5ff71b6e88108f9b9f39baae23170e56d39a
Saverio08 <situyuhui2008@gmail.com>
2025-07-13 17:55:20 +0200

    提交进度

---
+++ src/main/java/org/saverio/golditemexpansion/mixin/LivingEntityMixin.java
@@ -0,0 +1,21 @@
+package org.saverio.golditemexpansion.mixin;
+
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import org.saverio.golditemexpansion.effect.ModEffects;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(LivingEntity.class)
+public abstract class LivingEntityMixin {
+    @Inject(method = "addStatusEffect*", at = @At("HEAD"))
+    private void beforeAddGodEffect(StatusEffectInstance effect, CallbackInfoReturnable<Boolean> cir) {
+        if (effect.getEffectType() == ModEffects.GOD_STATUS_EFFECT) {
+            LivingEntity self = (LivingEntity) (Object) this;
+            self.removeStatusEffect(ModEffects.GOD_POSITIVE_EFFECT);
+            self.removeStatusEffect(ModEffects.GOD_NEGATIVE_EFFECT);
+        }
+    }
+}
