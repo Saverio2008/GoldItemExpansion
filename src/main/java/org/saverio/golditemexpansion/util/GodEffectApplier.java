ea92d3e059c2772a5dc09dde0bac518b2a52a145
Saverio08 <situyuhui2008@gmail.com>
2025-07-13 16:16:23 +0200

    提交进度

--- src/main/java/org/saverio/golditemexpansion/util/GodEffectApplier.java
+++ src/main/java/org/saverio/golditemexpansion/util/GodEffectApplier.java
@@ -5,7 +5,6 @@ import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectInstance;
 
 import java.util.Map;
-import java.util.Objects;
 
 public interface GodEffectApplier {
     Map<StatusEffect, Integer> getGodEffects();
@@ -16,28 +15,13 @@ public interface GodEffectApplier {
             entity.addStatusEffect(new StatusEffectInstance(effect, duration, amplifier, false, false,false));
         }
     }
-    @SuppressWarnings("ReassignedVariable")
     default void removeGodSubEffects(LivingEntity entity) {
-        var server = Objects.requireNonNull(entity.getServer());
-        server.execute(() -> {
-            // 延迟 5 tick 执行，确保避开状态遍历
-            for (int i = 0; i < 5; i++) {
-                server.execute(() -> {
-                });
-            }
-            server.execute(() -> {
-                var currentEffects = new java.util.ArrayList<>(entity.getStatusEffects());
-                for (StatusEffect effect : getGodEffects().keySet()) {
-                    boolean hasEffect = false;
-                    for (StatusEffectInstance inst : currentEffects) {
-                        if (inst.getEffectType() == effect) {
-                            hasEffect = true;
-                            break;
-                        }
-                    }
-                    if (hasEffect) entity.removeStatusEffect(effect);
+        TickDelayExecutor.runLater(entity.getServer(), 5, () -> {
+            for (StatusEffect effect : getGodEffects().keySet()) {
+                if (entity.hasStatusEffect(effect)) {
+                    entity.removeStatusEffect(effect);
                 }
-            });
+            }
         });
     }
 }
