39ec37f39b1efc094ace71585401e0411423fa64
Saverio08 <situyuhui2008@gmail.com>
2025-07-13 16:11:23 +0200

    提交进度

--- src/main/java/org/saverio/golditemexpansion/util/GodEffectApplier.java
+++ src/main/java/org/saverio/golditemexpansion/util/GodEffectApplier.java
@@ -18,20 +18,26 @@ public interface GodEffectApplier {
     }
     @SuppressWarnings("ReassignedVariable")
     default void removeGodSubEffects(LivingEntity entity) {
-        Objects.requireNonNull(entity.getServer()).execute(() -> {
-            var currentEffects = new java.util.ArrayList<>(entity.getStatusEffects());
-            for (StatusEffect effect : getGodEffects().keySet()) {
-                boolean hasEffect = false;
-                for (StatusEffectInstance inst : currentEffects) {
-                    if (inst.getEffectType() == effect) {
-                        hasEffect = true;
-                        break;
-                    }
-                }
-                if (hasEffect) {
-                    entity.removeStatusEffect(effect);
-                }
+        var server = Objects.requireNonNull(entity.getServer());
+        server.execute(() -> {
+            // 延迟 5 tick 执行，确保避开状态遍历
+            for (int i = 0; i < 5; i++) {
+                server.execute(() -> {
+                });
             }
+            server.execute(() -> {
+                var currentEffects = new java.util.ArrayList<>(entity.getStatusEffects());
+                for (StatusEffect effect : getGodEffects().keySet()) {
+                    boolean hasEffect = false;
+                    for (StatusEffectInstance inst : currentEffects) {
+                        if (inst.getEffectType() == effect) {
+                            hasEffect = true;
+                            break;
+                        }
+                    }
+                    if (hasEffect) entity.removeStatusEffect(effect);
+                }
+            });
         });
     }
 }
