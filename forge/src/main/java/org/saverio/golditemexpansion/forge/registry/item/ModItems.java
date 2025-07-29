63f4703cd7ed1ab80e9124ad5100b314fda1f1c6
Saverio08 <situyuhui2008@gmail.com>
2025-07-29 17:57:16 +0200

    wip

--- forge/src/main/java/org/saverio/golditemexpansion/forge/registry/item/ModItems.java
+++ forge/src/main/java/org/saverio/golditemexpansion/forge/registry/item/ModItems.java
@@ -1,24 +1,43 @@
 package org.saverio.golditemexpansion.forge.registry.item;
 
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.Item;
+import net.minecraft.world.item.*;
+import net.minecraftforge.common.util.MutableHashedLinkedMap;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
-import org.saverio.golditemexpansion.Golditemexpansion;
-import org.saverio.golditemexpansion.block.CompressedGoldBlock;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 
+import static org.saverio.golditemexpansion.Golditemexpansion.MOD_ID;
+
+@Mod.EventBusSubscriber(modid = MOD_ID, value = Dist.CLIENT, bus = Bus.MOD)
 public final class ModItems {
-    public static final DeferredRegister<Item> ITEMS =
-            DeferredRegister.create(ForgeRegistries.ITEMS, Golditemexpansion.MOD_ID);
 
-    @SuppressWarnings("unused")
-    public static final RegistryObject<Item> COMPRESSED_GOLD_BLOCK_ITEM =
-            ITEMS.register("compressed_gold_block", () ->
-                    new BlockItem(CompressedGoldBlock.COMPRESSED_GOLD_BLOCK, new Item.Properties())
-            );
-    public static void registerItems(IEventBus bus) {
-        ITEMS.register(bus);
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MOD_ID);
+
+    public static final RegistryObject<Item> COMPRESSED_GOLD_BLOCK_ITEM = ITEMS.register("compressed_gold_block", () ->
+            new Item(new Item.Properties())
+    );
+
+    public static void registerItems(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+
+    @SubscribeEvent
+    public static void addItemsToTab(BuildCreativeModeTabContentsEvent event) {
+        if (event.getTabKey().equals(CreativeModeTabs.BUILDING_BLOCKS)) {
+            MutableHashedLinkedMap<ItemStack, CreativeModeTab.TabVisibility> entries = event.getEntries();
+            for (var entry : entries) {
+                ItemStack existing = entry.getKey();
+                if (existing.getItem() == Items.GOLD_BLOCK) {
+                    entries.putAfter(existing, new ItemStack(COMPRESSED_GOLD_BLOCK_ITEM.get()), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+                    break;
+                }
+            }
+        }
     }
 }
