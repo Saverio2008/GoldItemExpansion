79a431399b2029b1b535b2ffcd9c02c6e98f5e3d
Saverio08 <situyuhui2008@gmail.com>
2025-07-30 20:49:04 +0200

    wip

--- common/src/main/java/org/saverio/golditemexpansion/mixin/client/EffectRenderingInventoryScreenMixin.java
+++ common/src/main/java/org/saverio/golditemexpansion/mixin/client/EffectRenderingInventoryScreenMixin.java
@@ -1,7 +1,6 @@
 package org.saverio.golditemexpansion.mixin.client;
 
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.inventory.EffectRenderingInventoryScreen;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectInstance;
@@ -9,42 +8,38 @@ import net.minecraft.world.entity.player.Player;
 import org.saverio.golditemexpansion.effect.ModEffectInstances;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 import java.util.Collection;
+import java.util.stream.Collectors;
 
 import static org.saverio.golditemexpansion.effect.GodNegativeStatusEffect.GOD_NEGATIVE_EFFECTS;
 import static org.saverio.golditemexpansion.effect.GodPositiveStatusEffect.GOD_POSITIVE_EFFECTS;
 
 @Mixin(EffectRenderingInventoryScreen.class)
 public final class EffectRenderingInventoryScreenMixin {
-    @Inject(method = "renderEffects", at = @At("HEAD"), cancellable = true)
-    private void onRenderEffects(GuiGraphics guiGraphics, int i, int j, CallbackInfo ci) {
+    @ModifyVariable(
+            method = "renderEffects",
+            at = @At(value = "STORE", ordinal = 0)
+    )
+    private Collection<MobEffectInstance> modifyActiveEffects(Collection<MobEffectInstance> originalCollection) {
         Player player = Minecraft.getInstance().player;
-        if (player == null) return;
-
+        if (player == null) {
+            return originalCollection;
+        }
         boolean hidePositive = player.hasEffect(ModEffectInstances.GOD_POSITIVE_EFFECT);
         boolean hideNegative = player.hasEffect(ModEffectInstances.GOD_NEGATIVE_EFFECT);
 
-        Collection<MobEffectInstance> filtered = player.getActiveEffects().stream()
-                .filter(effectInstance -> {
-                    MobEffect effect = effectInstance.getEffect();
-                    if (effect.equals(ModEffectInstances.GOD_STATUS_EFFECT) ||
-                            effect.equals(ModEffectInstances.GOD_POSITIVE_EFFECT) ||
-                            effect.equals(ModEffectInstances.GOD_NEGATIVE_EFFECT)) {
-                        return false;
-                    } else if (hidePositive && GOD_POSITIVE_EFFECTS.containsKey(effect)) {
-                        return false;
-                    } else if (hideNegative && GOD_NEGATIVE_EFFECTS.containsKey(effect)) {
-                        return false;
-                    }
-                    return true;
-                })
-                .toList();
-
-        if (filtered.isEmpty()) {
-            ci.cancel();
-        }
+        return originalCollection.stream().filter(effectInstance -> {
+            MobEffect effect = effectInstance.getEffect();
+            if (effect.equals(ModEffectInstances.GOD_STATUS_EFFECT)) {
+                return false;
+            } else if (hidePositive && GOD_POSITIVE_EFFECTS.containsKey(effect)) {
+                return false;
+            } else if (hideNegative && GOD_NEGATIVE_EFFECTS.containsKey(effect)) {
+                return false;
+            }
+            return true;
+        }).collect(Collectors.toList());
     }
 }
