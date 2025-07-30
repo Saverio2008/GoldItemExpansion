64e2964c4cb4373249c69d1f28308d36e7130c16
Saverio08 <situyuhui2008@gmail.com>
2025-07-30 18:49:36 +0200

    wip

--- common/src/main/java/org/saverio/golditemexpansion/util/EffectRemovalStatusTracker.java
+++ common/src/main/java/org/saverio/golditemexpansion/util/EffectRemovalStatusTracker.java
@@ -5,7 +5,7 @@ import net.minecraft.world.entity.LivingEntity;
 import java.util.Map;
 import java.util.WeakHashMap;
 
-public class EffectRemovalStatusTracker {
+public final class EffectRemovalStatusTracker {
     private static final Map<LivingEntity, Boolean> removalStatusMap = new WeakHashMap<>();
 
     public static void setRemoving(LivingEntity entity, boolean removing) {
