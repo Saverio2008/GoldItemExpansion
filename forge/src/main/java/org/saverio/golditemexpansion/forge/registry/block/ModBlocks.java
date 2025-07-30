3f0c8fcc7e02de65312eeb886c03c739e6f22d55
Saverio08 <situyuhui2008@gmail.com>
2025-07-30 11:57:14 +0200

    wip

--- forge/src/main/java/org/saverio/golditemexpansion/forge/registry/block/ModBlocks.java
+++ forge/src/main/java/org/saverio/golditemexpansion/forge/registry/block/ModBlocks.java
@@ -7,6 +7,7 @@ import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import org.saverio.golditemexpansion.Golditemexpansion;
 import org.saverio.golditemexpansion.block.CompressedGoldBlock;
+import org.saverio.golditemexpansion.block.GoldenHeadBlock;
 
 public final class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS =
@@ -15,6 +16,10 @@ public final class ModBlocks {
     @SuppressWarnings("unused")
     public static final RegistryObject<Block> COMPRESSED_GOLD_BLOCK =
             BLOCKS.register("compressed_gold_block", () -> CompressedGoldBlock.COMPRESSED_GOLD_BLOCK);
+
+    @SuppressWarnings("unused")
+    public static final RegistryObject<Block> GOLDEN_HEAD_BLOCK =
+            BLOCKS.register("golden_head", () -> GoldenHeadBlock.GOLDEN_HEAD_BLOCK);
 
     public static void registerBlocks(IEventBus bus) {
         BLOCKS.register(bus);
