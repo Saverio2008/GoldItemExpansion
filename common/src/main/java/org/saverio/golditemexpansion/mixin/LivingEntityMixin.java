3cb9217e82271b21d43665bb64c9b6da88731b7c
Saverio08 <situyuhui2008@gmail.com>
2025-07-30 15:44:38 +0200

    wip

--- common/src/main/java/org/saverio/golditemexpansion/mixin/LivingEntityMixin.java
+++ common/src/main/java/org/saverio/golditemexpansion/mixin/LivingEntityMixin.java
@@ -4,14 +4,24 @@ import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import org.jetbrains.annotations.Nullable;
+import org.saverio.golditemexpansion.effect.ModEffectInstances;
 import org.saverio.golditemexpansion.event.EffectChangeListenerManager;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LivingEntity.class)
-public abstract class LivingEntityEffectEventsMixin {
+public abstract class LivingEntityMixin {
+    @Inject(method = "addEffect(Lnet/minecraft/world/effect/MobEffectInstance;Lnet/minecraft/world/entity/Entity;)Z",
+            at = @At("HEAD"))
+    private void beforeAddGodEffectWithSource(MobEffectInstance effect, Entity source, CallbackInfoReturnable<Boolean> cir) {
+        if (effect.getEffect() == ModEffectInstances.GOD_STATUS_EFFECT) {
+            LivingEntity self = (LivingEntity)(Object)this;
+            self.removeEffect(ModEffectInstances.GOD_STATUS_EFFECT);
+        }
+    }
 
     @Inject(method = "onEffectAdded", at = @At("TAIL"))
     private void onEffectAddedInject(MobEffectInstance effectInstance, @Nullable Entity entity, CallbackInfo ci) {
