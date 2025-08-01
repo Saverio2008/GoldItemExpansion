632530d82acc88c78b2a6a4aaf6e116e27807ea9
Saverio08 <situyuhui2008@gmail.com>
2025-08-01 18:00:18 +0200

    wip

--- common/src/main/java/org/saverio/golditemexpansion/mixin/LivingEntityMixin.java
+++ common/src/main/java/org/saverio/golditemexpansion/mixin/LivingEntityMixin.java
@@ -1,5 +1,6 @@
 package org.saverio.golditemexpansion.mixin;
 
+import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
@@ -8,13 +9,21 @@ import org.saverio.golditemexpansion.effect.ModEffectInstances;
 import org.saverio.golditemexpansion.event.EffectChangeListenerManager;
 import org.saverio.golditemexpansion.util.GodEffectRemoveSkipManager;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin {
+    @Unique
+    private static final Map<LivingEntity, Long> golditemexpansion$lastRemoveEffectTick = new ConcurrentHashMap<>();
+    @Unique
+    private static final Map<LivingEntity, Boolean> golditemexpansion$removeEffectLock = new ConcurrentHashMap<>();
     @Inject(
             method = "addEffect(Lnet/minecraft/world/effect/MobEffectInstance;Lnet/minecraft/world/entity/Entity;)Z",
             at = @At("HEAD")
@@ -55,5 +64,30 @@ public abstract class LivingEntityMixin {
         LivingEntity self = (LivingEntity)(Object)this;
         GodEffectRemoveSkipManager.setSkip(self, false);
         GodEffectRemoveSkipManager.setForgeSkip(self, false);
+    }
+
+    @Inject(method = "removeEffectNoUpdate", at = @At("HEAD"))
+    private void onRemoveEffectNoUpdateHead(@Nullable MobEffect mobEffect, CallbackInfoReturnable<MobEffectInstance> cir) {
+        LivingEntity self = (LivingEntity)(Object)this;
+        long currentTick = self.level().getGameTime();
+
+        Long lastTick = golditemexpansion$lastRemoveEffectTick.get(self);
+        if (lastTick != null && (currentTick - lastTick) <= 10) {
+            golditemexpansion$removeEffectLock.put(self, true);
+        }
+        golditemexpansion$lastRemoveEffectTick.put(self, currentTick);
+
+        if (Boolean.TRUE.equals(golditemexpansion$removeEffectLock.get(self))) {
+            GodEffectRemoveSkipManager.setForgeSkip(self, true);
+        }
+    }
+
+    @Inject(method = "removeEffectNoUpdate", at = @At("RETURN"))
+    private void onRemoveEffectNoUpdateReturn(@Nullable MobEffect mobEffect, CallbackInfoReturnable<MobEffectInstance> cir) {
+        LivingEntity self = (LivingEntity)(Object)this;
+        if (Boolean.TRUE.equals(golditemexpansion$removeEffectLock.get(self))) {
+            GodEffectRemoveSkipManager.setForgeSkip(self, false);
+            golditemexpansion$removeEffectLock.remove(self);
+        }
     }
 }
