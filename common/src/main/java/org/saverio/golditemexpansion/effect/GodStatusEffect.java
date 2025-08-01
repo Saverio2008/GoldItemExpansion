17fe2832cfbeca344761f1afc5012989a9d454fb
Saverio08 <situyuhui2008@gmail.com>
2025-08-01 09:47:24 +0200

    wip

--- common/src/main/java/org/saverio/golditemexpansion/effect/GodStatusEffect.java
+++ common/src/main/java/org/saverio/golditemexpansion/effect/GodStatusEffect.java
@@ -35,8 +35,6 @@ public final class GodStatusEffect extends MobEffect {
             default -> isPositive = true;
         }
         MobEffect childEffect = isPositive ? GOD_POSITIVE_EFFECT : GOD_NEGATIVE_EFFECT;
-        entity.forceAddEffect(new MobEffectInstance(childEffect, duration, 0, false, false, true),
-                entity
-        );
+        entity.addEffect(new MobEffectInstance(childEffect, duration, 0, false, false, true));
     }
 }
